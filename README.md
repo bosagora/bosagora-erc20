6235b151cd53f2ca19e1d443579af90874c99da2
TrustHenry <hyeonyeob.kim@bpfkorea.org>
2019-05-13 17:32:07 +0900

    BOSAGORA First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# bosagora-token
+#BOSAGORA token
+BOSAGORA is a standard ERC20 token.
+Made by bosplatformfoundation
---
+++ contracts/BOSAGORA.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+//
+// BOSAGORA is a standard ERC20 token.
+// Made by bosplatformfoundation
+//
+
+import "../openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract BOSAGORA is ERC20, ERC20Detailed {
+    uint8 public constant DECIMALS = 7;
+    uint256 public constant INITIAL_SUPPLY = 5421301301958463;
+
+    constructor () public ERC20Detailed("BOSAGORA", "BOA", DECIMALS) {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
